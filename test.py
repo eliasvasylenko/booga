f03839b4e8d44df5bfa0b8c2593d34b6823da227
Elias N Vasylenko <eliasvasylenko@gmail.com>
2020-04-04 22:26:17 +0100

    window resizing, fullscreen toggle, etc.

--- test.py
+++ test.py
@@ -3,33 +3,40 @@ from OpenGL import *
 from OpenGL.GL import *
 import ctypes
 from expr import Expression, Prop
+from enum import Enum
+from threading import Lock
 
 class Game:
     def __init__(self):
         init = SDL_Init(SDL_INIT_EVERYTHING)
         if init != 0:
             error = SDL_GetError()
-            raise Exception(f'SDL_Init failed {error}')
-
-        self._window = SDL_CreateWindow(
-            b'Hello, Lindsey!',
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_OPENGL)
-        if self._window == None:
-            error = SDL_GetError()
-            raise Exception(f'SDL_CreateWindow failed {error}')
-
-        SDL_ShowWindow(self._window)
+            raise Exception(f'{error}')
 
         self._init_video()
 
         self._init_joysticks()
-
-    def quit(self):
+    
+    def _quit(self):
         SDL_DestroyWindow(self._window)
         SDL_Quit()
 
+    def _init_window(self):
+        self._window = SDL_CreateWindow(
+            b'Hello, Lindsey!',
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)
+        if self._window == None:
+            error = SDL_GetError()
+            raise Exception(f'{error}')
+
+        SDL_ShowWindow(self._window)
+        self._fullscreen_mode = SDL_WINDOW_FULLSCREEN_DESKTOP
+        self._refresh_window()
+
     def _init_video(self):
+        self._init_window()
+
         SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1)
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4)
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2)
@@ -39,19 +46,40 @@ class Game:
         self._maincontext = SDL_GL_CreateContext(self._window)
         if self._maincontext == None:
             error = SDL_GetError()
-            raise Exception(f'Failed to create OpenGL context {error}')
+            raise Exception(f'{error}')
 
         SDL_GL_SetSwapInterval(1)
 
+    def _on_window_event(self, window):
+        self._refresh_window()
+
+    def _set_fullscreen_real(self):
+        self._fullscreen_mode = SDL_WINDOW_FULLSCREEN
+        self._refresh_window()
+
+    def _set_fullscreen_fake(self):
+        self._fullscreen_mode = SDL_WINDOW_FULLSCREEN_DESKTOP
+        self._refresh_window()
+
+    def _toggle_fullscreen(self):
+        self._set_fullscreen(not self._is_fullscreen())
+
+    def _is_fullscreen(self):
+        return (SDL_GetWindowFlags(self._window) &
+            (SDL_WINDOW_FULLSCREEN | SDL_WINDOW_FULLSCREEN_DESKTOP)) != 0
+
+    def _set_fullscreen(self, setting):
+        if setting == self._is_fullscreen():
+            return
+        window_mode = self._fullscreen_mode if setting else 0
+        SDL_SetWindowFullscreen(self._window, window_mode)
+        self._refresh_window()
+
+    def _refresh_window(self):
         w = ctypes.c_int()
         h = ctypes.c_int()
         SDL_GetWindowSize(self._window, w, h)
-        print(f'Window size {w.value}, {h.value}')
-
         glViewport(0, 0, w.value, h.value)
-        glClearColor(0, 0.5, 1, 0)
-        glClear(GL_COLOR_BUFFER_BIT)
-        SDL_GL_SwapWindow(self._window)
 
     def _init_joysticks(self):
         js = joystick.SDL_NumJoysticks()
@@ -71,14 +99,19 @@ class Game:
                     self._on_key_down_event(event.key)
                 elif event.type == SDL_KEYUP:
                     self._on_key_up_event(event.key)
+    
+            self._render()
 
-        self.quit()
+        self._quit()
 
-    def _on_window_event(self, window):
-        pass
+    def _render(self):
+        glClearColor(0, 0.5, 1, 0)
+        glClear(GL_COLOR_BUFFER_BIT)
+        SDL_GL_SwapWindow(self._window)
 
     def _on_key_down_event(self, key):
-        pass
+        if key.keysym.sym == SDLK_F11:
+            self._toggle_fullscreen()
 
     def _on_key_up_event(self, key):
         pass
