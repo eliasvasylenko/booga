9dedeec0c777334fb4bc124bdea338849d180d53
Elias N Vasylenko <eliasvasylenko@gmail.com>
2020-04-06 13:46:40 +0100

    draws a rectangle to the screen using opengl 3.3

--- engine.py
+++ engine.py
@@ -37,8 +37,9 @@ class Engine:
         self._init_window()
 
         SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1)
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4)
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24)
 
--- mainmenu.py
+++ mainmenu.py
@@ -3,13 +3,25 @@ from engine import Scene
 
 from OpenGL import *
 from OpenGL.GL import *
-from OpenGL.GLEW import *
 from OpenGL.arrays import *
+from OpenGL.GL.shaders import *
 
 import numpy
 
 class MainMenu(Scene):
     def __init__(self):
+        self._vertex_shader = compileShader("""#version 330
+            in vec2 LVertexPos2D;
+            void main() {
+                gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 );
+            }""", GL_VERTEX_SHADER)
+
+        self._fragment_shader = compileShader("""#version 330
+            out vec4 LFragment;
+            void main() {
+                LFragment = vec4( 1.0, 1.0, 0.0, 1.0 );
+            }""", GL_FRAGMENT_SHADER)
+
         self._triangle = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, self._triangle)
         glBufferData(
@@ -18,23 +30,32 @@ class MainMenu(Scene):
                     [0.5, 0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5],
                     dtype='float32'),
                 GL_STATIC_DRAW)
+        
+        self._triangle_indices = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self._triangle_indices)
+        glBufferData(
+                GL_ELEMENT_ARRAY_BUFFER,
+                numpy.array(
+                    [0, 1, 2, 3],
+                    dtype='int32'),
+                GL_STATIC_DRAW)
+        
+        self._vao = glGenVertexArrays(1);
+        glBindVertexArray(self._vao);
 
-        vertex_shader = shaders.compileShader("""#version 140
-            in vec2 LVertexPos2D;
-            void main() {
-                gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 );
-            }""", GL_VERTEX_SHADER)
+        self._triangle_shader = compileProgram(self._vertex_shader, self._fragment_shader)
+        self._vertex_location = glGetAttribLocation(self._triangle_shader, 'LVertexPos2D')
 
-        fragment_shader = shaders.compileShader("""#version 140
-            out vec4 LFragment;
-            void main() {
-                LFragment = vec4( 1.0, 1.0, 1.0, 1.0 );
-            }""", GL_FRAGMENT_SHADER)
 
-        self._triangle_shader = shaders.compileProgram(vertex_shader, fragment_shader)
+        glVertexAttribPointer(
+                self._vertex_location, 2, GL_FLOAT, GL_FALSE,
+                2 * ctypes.sizeof(ctypes.c_float), None)
+        glEnableVertexAttribArray(self._vertex_location)
 
     def __del__(self):
         glDeleteBuffers(1, self._triangle)
+        glDeleteShader(self._vertex_shader)
+        glDeleteShader(self._fragment_shader)
 
     def process(self):
         self._render()
@@ -44,14 +65,11 @@ class MainMenu(Scene):
         glClear(GL_COLOR_BUFFER_BIT)
 
         glUseProgram(self._triangle_shader)
-        glEnableVertexAttribArray(pos2d)
         
-        glBindBuffer(GL_ARRAY_BUFFER, self._triangle)
-        glVertexAttribPointer(pos2d, 2, GL_FLAOT, GL_FALSE, 2 * 8, NULL)
+        glBindVertexArray(self._vao);
+
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self._triangle_indices)
-        glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL)
+        glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, None)
 
-        glDisableVertexAttribArray(pos2d)
-
-        glUseProgram(None)
+        glUseProgram(0)
 
